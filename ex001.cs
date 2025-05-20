084ca0dbe3a502e4ec7c58a1f95178c31819c3ae
Luis-H099 <danielfranesi69@gmail.com>
2025-05-20 15:59:38 -0300

    Segundo Commit

---
+++ ex001.cs
@@ -0,0 +1,14 @@
+Console.WriteLine("Informe o seu nome");
+string nome = Console.ReadLine();
+ 
+Console.WriteLine("Informe a sua idade");
+string idade = Console.ReadLine();
+ 
+Console.WriteLine("Informe a sua altura");
+string altura = Console.ReadLine();
+ 
+Console.WriteLine("Informe o seu peso");
+string peso = Console.ReadLine();
+ 
+Console.WriteLine($"Seu nome completo é {nome}, você tem {idade}, {altura} de altura e peso {peso}Kg");
+Console.ReadLine();
